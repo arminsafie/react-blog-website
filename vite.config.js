e52d065782dbde91a6242d0672ebfd88246ac1b4
arminsafie <arminsafaie92@gmail.com>
2026-01-06 17:45:13 +0330

    ff2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-
+import path from "path"; // needed for aliases
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
